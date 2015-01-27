466a0be886c0ac65531869eb83811e7d5ad5e0cc
Dave Syer <dsyer@pivotal.io>
2015-01-27 09:05:50 +0000

    Fix package names

--- zuul-proxy/src/test/java/demo/ZuulProxyApplicationTests.java
+++ zuul-proxy/src/test/java/demo/ZuulProxyApplicationTests.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.NoopDiscoveryClient;
+import org.springframework.cloud.client.discovery.noop.NoopDiscoveryClient;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
