01f4c089432150fce10d835bb110cfa5e9e5e6bf
Dave Syer <dsyer@pivotal.io>
2015-02-05 10:09:33 +0100

    Add turbine sample

---
+++ turbine/src/main/java/demo/TurbineApplication.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+public class TurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class, args);
+    }
+}
