b607516ed95d490e30f4bc1a0d244dd5630f40df
Dave Syer <dsyer@pivotal.io>
2015-02-27 18:01:16 +0000

    Add a couple of new integration test samples
    configserver-eureka: a vanilla config server that is a eureka client
    eureka-first: a eureka client that diecovers the config server
    through eureka

---
+++ eureka-first/src/main/java/demo/EurekaFirstApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaFirstApplication.class, args);
+    }
+}
