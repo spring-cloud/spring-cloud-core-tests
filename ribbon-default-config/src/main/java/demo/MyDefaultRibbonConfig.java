f7975c91e8394d1a05a551ad3fdd6d9a5748ca78
Spencer Gibb <spencer@gibb.us>
2015-06-08 14:02:55 -0600

    added sample test that make sure a users default ribbon config is used even if using autoconfig

---
+++ ribbon-default-config/src/main/java/demo/MyDefaultRibbonConfig.java
@@ -0,0 +1,48 @@
+package demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.BestAvailableRule;
+import com.netflix.loadbalancer.ConfigurationBasedServerList;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.ServerList;
+import com.netflix.loadbalancer.ServerListSubsetFilter;
+
+/**
+ * @author brenuart
+ *
+ */
+@Configuration
+public class MyDefaultRibbonConfig {
+	@Bean
+	public IRule ribbonRule() {
+		return new BestAvailableRule();
+	}
+
+	@Bean
+	public IPing ribbonPing() {
+		return new PingUrl();
+	}
+
+	@Bean
+	public ServerList<Server> ribbonServerList(IClientConfig config) {
+		return new MyDefaultRibbonConfig.BazServiceList(config);
+	}
+
+	@Bean
+	public ServerListSubsetFilter serverListFilter() {
+		ServerListSubsetFilter filter = new ServerListSubsetFilter();
+		return filter;
+	}
+	
+	public static class BazServiceList extends ConfigurationBasedServerList {
+		public BazServiceList(IClientConfig config) {
+			super.initWithNiwsConfig(config);
+		}
+	}
+}
---
+++ ribbon-default-config/src/main/java/demo/RibbonDefaultConfigApplication.java
@@ -0,0 +1,63 @@
+package demo;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.ServerList;
+import com.netflix.loadbalancer.ZoneAwareLoadBalancer;
+
+/**
+ * @author brenuart
+ */
+@ComponentScan(
+		// Exclude @Configuration classes that should be included only in sub contexts created
+		// by Ribbon's SpringClientFactory.
+		excludeFilters={
+				@ComponentScan.Filter(type=FilterType.REGEX, pattern=".*RibbonConfig")
+		}
+	)
+
+@SpringBootApplication
+@RibbonClients(defaultConfiguration=MyDefaultRibbonConfig.class)
+@Configuration
+public class RibbonDefaultConfigApplication {
+
+	@Autowired
+	private SpringClientFactory clientFactory;
+
+	/**
+	 * Throws exception if the SpringClientFactory doesn't return a balancer with a server list
+	 * of the expected type.
+	 *
+	 */
+    @PostConstruct
+    public void test() throws Exception {
+    	ZoneAwareLoadBalancer<Server> lb = (ZoneAwareLoadBalancer<Server>)clientFactory.getLoadBalancer("baz");
+
+    	ServerList<Server> serverList= lb.getServerListImpl();
+    	if( !(serverList instanceof MyDefaultRibbonConfig.BazServiceList) ) {
+    		throw new Exception("wrong server list type");
+    	}
+    }
+
+
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+    	SpringApplication app = new SpringApplicationBuilder(RibbonDefaultConfigApplication.class).build();
+    	app.run(args);
+    }
+}
+
