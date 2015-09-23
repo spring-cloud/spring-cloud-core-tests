f028e47fe78a197e542a4924659b0742d3eff66d
Dave Syer <dsyer@pivotal.io>
2015-09-23 16:49:17 +0100

    Refactor test for changes in eureka-first

--- eureka-first/src/main/java/demo/EurekaFirstApplication.java
+++ eureka-first/src/main/java/demo/EurekaFirstApplication.java
@@ -2,8 +2,10 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaFirstApplication {
 
     public static void main(String[] args) {
