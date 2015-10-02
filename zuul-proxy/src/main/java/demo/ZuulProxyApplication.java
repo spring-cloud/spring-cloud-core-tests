16de62aed5c38f560a2411acb21dc937dc695091
Dave Syer <dsyer@pivotal.io>
2015-10-02 15:41:25 +0100

    Fix whitepspace

--- zuul-proxy/src/main/java/demo/ZuulProxyApplication.java
+++ zuul-proxy/src/main/java/demo/ZuulProxyApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 public class ZuulProxyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ZuulProxyApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyApplication.class, args);
+	}
 }
