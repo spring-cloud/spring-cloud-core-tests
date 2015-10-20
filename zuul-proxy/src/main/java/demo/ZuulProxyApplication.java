944a8691e2a76e20b2895de201db831195ee0139
Dave Syer <dsyer@pivotal.io>
2015-10-20 09:24:28 -0400

    Add tests for forwards

--- zuul-proxy/src/main/java/demo/ZuulProxyApplication.java
+++ zuul-proxy/src/main/java/demo/ZuulProxyApplication.java
@@ -3,10 +3,23 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableZuulProxy
+@RestController
 public class ZuulProxyApplication {
+
+	@RequestMapping("/good")
+	public String good() {
+		return "GOOD!";
+	}
+
+	@RequestMapping("/good/stuff")
+	public String stuff() {
+		return "STUFF!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulProxyApplication.class, args);
--- zuul-proxy/src/test/java/demo/ZuulProxyApplicationTests.java
+++ zuul-proxy/src/test/java/demo/ZuulProxyApplicationTests.java
@@ -1,32 +1,58 @@
 package demo;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.noop.NoopDiscoveryClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import apps.UiApplication;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = UiApplication.class)
+@SpringApplicationConfiguration(classes = ZuulProxyApplication.class)
 @DirtiesContext
+@WebIntegrationTest(randomPort=true)
 public class ZuulProxyApplicationTests {
 
 	@Autowired
 	DiscoveryClient discoveryClient;
 
+	@Value("${local.server.port}")
+	int port;
+
+	private TestRestTemplate restTemplate = new TestRestTemplate();
+
 	@Test
-	public void contextLoads() {
+	public void ignoredPatternMissing() {
+		ResponseEntity<String> result = this.restTemplate.getForEntity("http://localhost:"+this.port +"/missing", String.class);
+		assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
+	}
+
+	@Test
+	public void forwardedPatternGood() {
+		ResponseEntity<String> result = this.restTemplate.getForEntity("http://localhost:"+this.port +"/good", String.class);
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+		assertEquals("GOOD!", result.getBody());
+	}
+
+	@Test
+	public void forwardedPatternGoodWithPath() {
+		ResponseEntity<String> result = this.restTemplate.getForEntity("http://localhost:"+this.port +"/good/stuff", String.class);
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+		assertEquals("STUFF!", result.getBody());
 	}
 
 	@Test
 	public void discoveryClientIsNoop() {
-		assertTrue("discoveryClient is wrong type", discoveryClient instanceof NoopDiscoveryClient);
+		assertTrue("discoveryClient is wrong type", this.discoveryClient instanceof NoopDiscoveryClient);
 	}
 }
