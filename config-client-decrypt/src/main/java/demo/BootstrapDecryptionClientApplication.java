6c8507d73eec12bfdcb50fe25b640a819f0ed35d
Dave Syer <dsyer@pivotal.io>
2015-12-17 21:21:08 +0000

    Add simple configserver

--- config-client-decrypt/src/main/java/demo/BootstrapDecryptionClientApplication.java
+++ config-client-decrypt/src/main/java/demo/BootstrapDecryptionClientApplication.java
@@ -7,21 +7,27 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.client.ConfigClientProperties;
+import org.springframework.core.env.Environment;
 
 /**
  * @author Dave Syer
  */
 @SpringBootApplication
 public class BootstrapDecryptionClientApplication implements CommandLineRunner {
-	
+
 	private static Log logger = LogFactory.getLog(BootstrapDecryptionClientApplication.class);
-	
+
 	@Autowired
 	private ConfigClientProperties config;
-	
+
+	@Autowired
+	private Environment environment;
+
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("Config Server URI: " + config.getUri());
+		logger.info("Config Server URI: " + this.config.getUri());
+		logger.info("And info.bar: " + this.environment.getProperty("info.bar"));
+		logger.info("And info.spam: " + this.environment.getProperty("info.spam"));
 	}
 
 	public static void main(String[] args) {
---
+++ configserver/src/test/java/demo/ConfigServerApplicationTests.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ConfigServerApplication.class)
+@WebAppConfiguration
+@DirtiesContext
+public class ConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
