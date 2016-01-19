2cd7e1a54d0a356fe32a1af9b309463453d333b7
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-01-19 11:42:13 +0100

    Fixed Sleuth compilation issues

--- sleuth/src/test/java/demo/TraceApplicationTests.java
+++ sleuth/src/test/java/demo/TraceApplicationTests.java
@@ -1,16 +1,16 @@
 package demo;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.cloud.sleuth.TraceManager;
-import org.springframework.cloud.sleuth.trace.DefaultTraceManager;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.trace.DefaultTracer;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TraceApplication.class)
@@ -19,7 +19,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 public class TraceApplicationTests {
 
 	@Autowired
-	TraceManager traceManager;
+	Tracer traceManager;
 
 	@Test
 	public void contextLoads() {
@@ -27,6 +27,6 @@ public class TraceApplicationTests {
 
 	@Test
 	public void traceIsDefaultTrace() {
-		assertTrue("trace is wrong type", this.traceManager instanceof DefaultTraceManager);
+		assertTrue("trace is wrong type", this.traceManager instanceof DefaultTracer);
 	}
 }
