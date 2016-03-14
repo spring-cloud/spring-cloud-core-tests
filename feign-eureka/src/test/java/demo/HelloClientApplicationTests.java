aa85eb8aeb90ee185dffd1a7f7900bbb58042126
Spencer Gibb <spencer@gibb.us>
2016-03-14 11:23:13 -0600

    Update tests to use @WebIntegrationTest

--- feign-eureka/src/test/java/demo/HelloClientApplicationTests.java
+++ feign-eureka/src/test/java/demo/HelloClientApplicationTests.java
@@ -1,25 +1,23 @@
 package demo;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = HelloClientApplication.class)
-@WebAppConfiguration
-@IntegrationTest("server.port=0")
+@WebIntegrationTest(randomPort = true)
 @DirtiesContext
 public class HelloClientApplicationTests {
 
--- feign-eureka/src/test/java/demo/NoWebApplicationTests.java
+++ feign-eureka/src/test/java/demo/NoWebApplicationTests.java
@@ -1,20 +1,20 @@
 package demo;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.noop.NoopDiscoveryClient;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = HelloClientApplication.class)
-@IntegrationTest("eureka.client.enabled=false")
+@WebIntegrationTest(value = "eureka.client.enabled=false", randomPort = true)
 @DirtiesContext
 public class NoWebApplicationTests {
 
