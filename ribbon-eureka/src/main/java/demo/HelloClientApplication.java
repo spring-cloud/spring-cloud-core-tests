9fccb780531180e83aa5be37cfd174d5d65ae426
Spencer Gibb <spencer@gibb.us>
2016-03-14 11:22:44 -0600

    Add missing RestTemplate

--- ribbon-eureka/src/main/java/demo/HelloClientApplication.java
+++ ribbon-eureka/src/main/java/demo/HelloClientApplication.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -27,4 +29,9 @@ public class HelloClientApplication {
 		SpringApplication.run(HelloClientApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
