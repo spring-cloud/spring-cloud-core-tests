222244332b5f2d611491845695f644678c146e0a
spencergibb <spencer@gibb.us>
2020-08-19 11:39:45 -0400

    Ignores test that uses wrong boot version on jenkins.
    See gh-39

--- configserver-bootstrap/src/test/java/demo/ConfigServerBootstrapApplicationTests.java
+++ configserver-bootstrap/src/test/java/demo/ConfigServerBootstrapApplicationTests.java
@@ -1,5 +1,6 @@
 package demo;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ConfigServerBootstrapApplicationTests {
 
 	@Test
+	@Ignore // see https://github.com/spring-cloud/spring-cloud-core-tests/issues/39
 	public void contextLoads() {
 	}
 
