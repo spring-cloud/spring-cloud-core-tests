b962e8b5f0d16eb2cef39e26ce723df0a89d4f0c
spencergibb <spencer@gibb.us>
2021-01-11 12:09:42 -0500

    Unignores test
    Fixes gh-39

--- configserver-bootstrap/src/test/java/demo/ConfigServerBootstrapApplicationTests.java
+++ configserver-bootstrap/src/test/java/demo/ConfigServerBootstrapApplicationTests.java
@@ -1,8 +1,8 @@
 package demo;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,7 +13,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ConfigServerBootstrapApplicationTests {
 
 	@Test
-	@Ignore // see https://github.com/spring-cloud/spring-cloud-core-tests/issues/39
 	public void contextLoads() {
 	}
 
