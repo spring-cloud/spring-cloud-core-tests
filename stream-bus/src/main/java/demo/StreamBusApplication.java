474cb1365da5b58a2ec4511eafb0dcc9258b87f0
spencergibb <sgibb@vmware.com>
2022-01-21 13:42:59 -0500

    Updates to spring cloud 2022.0.0
    Includes updates to jdk17, framework 6 and boot 3

--- stream-bus/src/main/java/demo/StreamBusApplication.java
+++ stream-bus/src/main/java/demo/StreamBusApplication.java
@@ -2,8 +2,6 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
 
 /**
  * @author Dave Syer
